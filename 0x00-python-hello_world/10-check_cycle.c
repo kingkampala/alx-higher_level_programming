9f5f20f902e432aec47bd8ab9a61df5828b4e4d8
kampala <ezeanyathankgod@gmail.com>
2022-08-22 18:10:33 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for cycle in linked list
+ * list: linked list to check
+ * Return: 0 if there is no cycle, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
