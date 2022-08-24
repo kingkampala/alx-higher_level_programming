a8bf52ee63391bf75d7a881c7147d0624601e85a
kampala <ezeanyathankgod@gmail.com>
2022-08-24 05:08:47 -0700

    1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+king = abs(number) % 10
+if number < 0:
+    king = -king
+print("Last digit of {:d} is {} and is ",.format(number, king), end="")
+if king > 5:
+    print("greater than 5")
+elif king == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
