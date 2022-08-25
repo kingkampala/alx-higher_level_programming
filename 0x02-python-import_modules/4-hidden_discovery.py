7e0debb6106ea8d4e130559dd658cf46411b8a84
kampala <ezeanyathankgod@gmail.com>
2022-08-25 09:43:58 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     name = dir(hidden_4)
-    for n in mame:
-        if n[:2] != "__":
-            print(n)
+    for n in name:
+        if n[:2] != '__':
+            print("{:s}".format(n))
