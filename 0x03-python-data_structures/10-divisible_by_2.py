77090b9f9d88f76f7381c8b8a4e90a81e3f464d0
kampala <ezeanyathankgod@gmail.com>
2022-08-29 04:20:41 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    king = my_list[:]
+    for count, n in enumerate(my_list):
+        if n % 2 == 0:
+            king[count] = True
+        else:
+            king[count] = False
+        return (king)
